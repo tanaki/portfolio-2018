39b9abb5bb72ae28958c30206c46271c0c3850b8
Nicolas Pigelet <nico@nicolaspigelet.com>
2018-06-05 14:22:09 +0200

    more content + style

--- src/components/WorkDetail.vue
+++ src/components/WorkDetail.vue
@@ -8,13 +8,14 @@
     </div>
 
     <div class="block block-client">
-      <a :href="project.company_detail" target="_blank">{{project.company}}</a><br>
-      {{project.context}}
+      build with <a :href="project.company_detail" target="_blank">{{project.company}}</a><br>
+      as a {{project.context}}
     </div>
 
     <div v-if="project.links" class="block block-links">
       <div v-for="(link, index) in project.links" :key="index">
-        <a :href="link.src" target="_blank">{{link.label}}</a>
+        <a v-if="link.src" :href="link.src" target="_blank">{{link.label}}</a>
+        <span v-else>{{link.label}}</span>
       </div>
     </div>
 
@@ -24,9 +25,10 @@
 
     <div v-if="project.awards" class="block block-awards">
       <h3>Awards</h3>
-      <div v-for="(award, index) in project.awards" :key="index">
-        <p>{{award.year}} - {{award.label}}</p>
-        <p>{{award.category}}</p>
+      <div v-for="(award, index) in project.awards" :key="index" class="award-detail">
+        <p class="year">{{award.year}}</p>
+        <p class="label">{{award.label}}</p>
+        <p class="category">{{award.category}}</p>
       </div>
     </div>
 
